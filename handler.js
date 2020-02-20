3686b9f4ed4acdbb24f9a85e1719004619813925
Bilal Shah <mbilalshah1@gmail.com>
2020-02-20 16:47:12 +0500

    initial commit

--- handler.js
+++ handler.js
@@ -1,27 +1,19 @@
 import express from 'express';
 import serverless from 'serverless-http';
 
-export const app = express();
+const app = express();
 
-// export const hello = async (event, context) => {
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify({
-//       message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
-//     }),
-//   };
-// };
-
-// const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
-//   setTimeout(() => {
-//     resolve(`${rest.copy} (with a delay)`);
-//   }, time * 1000)
-// );
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send("hello world");
 });
 
-export const hello = serverless(app);
+
+const hello = serverless(app);
+export {
+  app, 
+  hello
+};
